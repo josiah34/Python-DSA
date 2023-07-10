b9c2c93954e6206a4f4e4dbdc5e0f879f9f3a842
Josiah <josiah_g@live.com>
2023-07-09 21:50:56 -0400

    Big O
    Finished

---
+++ big-o/big-o.md
@@ -0,0 +1,33 @@
+## Big O 
+
+**Big O** is used to describe the performance or efficiency of an algorithm. 
+
+
+## Time complexity 
+
+**Time Complexity** is a measure of the amount of time taken by an algorithm to run, typically expressed in terms of input size. 
+
+## Space Complexity** 
+
+**Space Complexity** is a measure of the amount of memory or storage space required by an algorithm to solve problem. 
+
+
+
+<hr>
+
+**Omega**(Ω): refers to the best case scenario of an algorithms runtime. 
+
+**Theta**(θ): refers to the average case. 
+
+**Omicron**(O): refers to the worst case. 
+
+
+## Terms 
+
+Loop within a Loop - O(n^2)
+
+Proportional - O(n)
+
+Divide and Conquer - O(log n)
+
+Constant - O(1)
---
+++ big-o/big-o.py
@@ -0,0 +1,86 @@
+## Code for Big O Notation
+
+
+
+### O(n) 
+
+
+
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+
+
+## Ran n operations
+# print_items(10)
+
+
+
+## Drop constants 
+
+
+def print_items_drop(n):
+    for i in range(n):
+        print(i)
+
+    for i in range(n):
+        print(i)
+        
+# Ran two of n times        (n+n)
+# This would not be written as O(2n) but rather O(n)
+# We drop the constant
+# print_items_drop(10)
+
+
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print(i,j)
+            
+
+# Ran n^2 times
+# The inner loop runs n times for every iteration of the outer loop
+# N * N = N^2
+# O (n^2)
+
+# print_items(20)
+
+
+
+## Drop non-dominant terms
+
+def print_items(n):
+    ## These two loops are O(n^2)
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+    ## This loop is O(n)
+    for k in range(n):
+        print(k)
+
+## The loop on the outside of of O(n) is the non-dominant term and is dropped
+## So O(n^2 + n) becomes O(n^2)
+# print_items(20)
+
+
+## O(1) - Constant Time
+
+# def add_items(n):
+#     return n + n + n
+
+## This function will always run 3 operations no matter the size of n. So it is O(1). The value of n does not create any more operations. 
+# print(add_items(10))
+
+## This is the most efficient Big O notation. It is constant time.
+
+
+
+### Terms
+
+Loop within a Loop - O(n^2)
+
+Proportional - O(n)
+
+Divide and Conquer - O(log n)
+
+Constant - O(1)
