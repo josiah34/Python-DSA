6f1038ab609a84584beda8d22389eceb1a523bcd
Josiah <josiah_g@live.com>
2023-07-13 11:00:35 -0400

    Update linkedlist.py

--- linked-lists/linkedlist.py
+++ linked-lists/linkedlist.py
@@ -1,3 +1,11 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+
 class LinkedList:
     def __init__(self, value):
         # Creates a new node with the value passed in
@@ -17,10 +25,9 @@ class LinkedList:
         # Creates a new node with the value passed in
         new_node = Node(value)
         # If list is empty the head and tail will point to the new node
-        if self.head == None:
+        if self.head == 0:
             self.head = new_node
             self.tail = new_node
-            print("Value added")
         else:
             # If the list is not empty add the new node to the end of the list and shift the tail to point to the new node
             self.tail.next = new_node
@@ -47,24 +54,24 @@ class LinkedList:
             self.tail = None
         # Return the deleted node object
         return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node 
+            self.tail = new_node
+        else:
+            # Set the 
+            new_node.next = self.head
+            self.head = new_node 
+            print(f"Value added to the front of the list: {self.head.value}")
+        
+            
 
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
 
 
 my_linked_list = LinkedList(4)
+my_linked_list.append(6)
+my_linked_list.prepend(9)
+my_linked_list.print_list()
 
-
-my_linked_list.append(2)
-
-
-print(my_linked_list.head.value)
-
-print(my_linked_list.tail.value)
-
-print(my_linked_list.print_list())
-
-print(my_linked_list.pop())
