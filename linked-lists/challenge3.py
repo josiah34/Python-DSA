9e5c9497d0b7700577de66ffa188f1f46a6fedd7
Josiah <josiah_g@live.com>
2023-07-31 18:12:28 -0400

    Finished challenge

---
+++ linked-lists/challenge3.py
@@ -0,0 +1,73 @@
+
+## Linked Lists - Challenge 3 Remove Duplicates ## 
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next  
+            
+    def print_all(self):
+        if self.length == 0:
+            print("Head: None")
+        else:
+            print("Head: ", self.head.value)
+        print("Length: ", self.length)
+        print("\nLinked List:")
+        if self.length == 0:
+            print("empty")
+        else:
+            self.print_list()
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+    
+    def remove_duplicates(self):
+        values = ()
+        prev = None 
+        current = self.head
+        if self.length == 0:
+            return False
+        while current is not None:
+            if current.value in values:
+                prev.next = current.next
+                self.length -= 1
+            else:
+                values += (current.value,)
+                prev = current
+            current = current.next
+    
+        
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(3)
+my_linked_list.append(2)
+my_linked_list.append(4)
+my_linked_list.remove_duplicates()
+
+my_linked_list.print_all()
